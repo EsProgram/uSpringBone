e3402a31e687b4818b9708ffd3dafd7bfbcd3b52
shigehiro-don <shigehiro.yoshiki@donuts.ne.jp>
2018-10-01 14:07:48 +0900

    to ECS.

---
+++ Assets/uSpringBone/Script/Core/SphereColliderComponent.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Entities;
+
+namespace Es.uSpringBone
+{
+    /// <summary>
+    /// Sphere collider data.
+    /// </summary>
+    [System.Serializable]
+    public struct SphereColliderData : IComponentData
+    {
+        [HideInInspector]
+        public Entity entity;
+        [HideInInspector]
+        public float radius;
+
+        public SphereColliderData(Entity entity, float radius)
+        {
+            this.entity = entity;
+            this.radius = radius;
+        }
+    }
+
+    /// <summary>
+    /// Collision determination component.
+    /// </summary>
+    [ScriptExecutionOrder(-30000)]
+    [RequireComponent(typeof(PositionComponent), typeof(CopyTransformFromGameObjectComponent))]
+    public class SphereColliderComponent : ComponentDataWrapper<SphereColliderData>
+    {
+        public float radius;
+
+        public void Initialize()
+        {
+            var gameObjectEntity = GetComponent<GameObjectEntity>();
+            Value = new SphereColliderData(gameObjectEntity.Entity, radius);
+        }
+
+        // TODO: radiusが変わった時のみentityをupdateするように
+        // void Update()
+        // {
+        //     var tmp = Value;
+        //     tmp.radius = radius;
+        //     Value = tmp;
+        // }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            var tmp = Gizmos.color;
+            Gizmos.color = new Color(1f, 0.92f, 0.016f, 0.8f);
+            Gizmos.DrawSphere(transform.position, radius);
+            Gizmos.color = tmp;
+        }
+#endif
+    }
+}
---
+++ Assets/uSpringBone/Script/Core/SpringBoneComponent.cs
@@ -0,0 +1,179 @@
+﻿using UnityEngine;
+using Unity;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using System;
+using System.Collections;
+using System.Linq;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Es.uSpringBone
+{
+    /// <summary>
+    /// SpringBone data.
+    /// </summary>
+    [Serializable]
+    public struct SpringBoneData : IComponentData
+    {
+        const int TRUE = 1;
+        [HideInInspector]
+        public Entity entity;
+        [HideInInspector]
+        public float3 localPosition;
+        [HideInInspector]
+        public float3 grobalPosition;
+        [HideInInspector]
+        public float3 currentEndpoint;
+        [HideInInspector]
+        public float3 previousEndpoint;
+        [HideInInspector]
+        public quaternion localRotation;
+        [HideInInspector]
+        public quaternion grobalRotation;
+        [HideInInspector]
+        public float3 parentScale;
+        [HideInInspector]
+        public float3 boneAxis;
+        [HideInInspector]
+        public float radius;
+        [HideInInspector]
+        public float stiffnessForce;
+        [HideInInspector]
+        public float dragForce;
+        [HideInInspector]
+        public float3 springForce;
+        [HideInInspector]
+        public float springLength;
+        [HideInInspector]
+        public int isRootChild;
+
+        public SpringBoneData(
+            Entity entity,
+            Vector3 localPosition,
+            Vector3 grobalPosition,
+            Vector3 currentEndpoint,
+            Vector3 previousEndpoint,
+            Quaternion localRotation,
+            Quaternion grobalRotation,
+            Vector3 parentScale,
+            Vector3 boneAxis,
+            float radius,
+            float stiffnessForce,
+            float dragForce,
+            Vector3 springForce,
+            float springLength,
+            int isRootChild
+        )
+        {
+            this.entity = entity;
+            this.localPosition = localPosition;
+            this.grobalPosition = grobalPosition;
+            this.currentEndpoint = currentEndpoint;
+            this.previousEndpoint = previousEndpoint;
+            this.localRotation = localRotation;
+            this.grobalRotation = grobalRotation;
+            this.parentScale = parentScale;
+            this.boneAxis = boneAxis;
+            this.radius = radius;
+            this.stiffnessForce = stiffnessForce;
+            this.dragForce = dragForce;
+            this.springForce = springForce;
+            this.springLength = springLength;
+            this.isRootChild = isRootChild;
+        }
+
+        /// <summary>
+        /// Whether it is the bone which becomes the root of the hierarchy.
+        /// </summary>
+        /// <returns>If true, it indicates that it is the root SpringBone.</returns>
+        public bool IsRootBone => isRootChild == TRUE;
+    }
+
+
+    /// <summary>
+    /// Has bone specific data.
+    /// </summary>
+    [RequireComponent(typeof(CopyTransformToGameObjectComponent))]
+    public class SpringBoneComponent : ComponentDataWrapper<SpringBoneData>
+    {
+        const int TRUE = 1;
+        const int FALSE = 0;
+
+        [HideInInspector, NonSerialized]
+        public Transform child;
+        public Vector3 boneAxis = new Vector3(-1.0f, 0.0f, 0.0f);
+        public float radius = 0.1f;
+        public float stiffnessForce = 0.05f;
+        public float dragForce = 2f;
+        public Vector3 springForce = new Vector3(0.0f, 0.0f, 0.0f);
+
+        [SerializeField]
+        Mesh debugMesh;
+
+        /// <summary>
+        /// Initialize SpringBone data.
+        /// </summary>
+        public void Initialize()
+        {
+            // get child.
+            if (child == null)
+                child = GetChild();
+
+            // FIXME : Entities 0.0.12-preview.17 BUG
+            //? CopyTransformToGameObjectComponent causes a bug when working on the Editor.
+            //? Specifically, since we always try to rewrite Transform with global coordinates, we can not move the parent object.
+            //? In addition, the local coordinates of Transform are rewritten to global coordinates, and the positional relationship becomes strange between parent and child.
+            //? For that reason, we do not attach PositionComponent / RotationComponent in the Editor but add it dynamically with AddComponent.
+            gameObject.AddComponent<PositionComponent>();
+            gameObject.AddComponent<RotationComponent>();
+
+            var gameObjectEntity = GetComponent<GameObjectEntity>();
+            var cachedTransform = transform;
+            var isRootChild = transform.parent.GetComponent<SpringBoneComponent>() == null ? TRUE : FALSE;
+
+            // make bone data.
+            Value = new SpringBoneData(
+                gameObjectEntity.Entity,
+                cachedTransform.localPosition,
+                cachedTransform.position,
+                child.position,
+                child.position,
+                cachedTransform.localRotation,
+                cachedTransform.rotation,
+                cachedTransform.parent.lossyScale,
+                boneAxis,
+                radius,
+                stiffnessForce,
+                dragForce,
+                springForce,
+                Vector3.Distance(cachedTransform.position, child.position),
+                isRootChild
+            );
+        }
+
+        /// <summary>
+        /// get child transform.
+        /// </summary>
+        /// <returns></returns>
+        private Transform GetChild()
+        {
+            return transform.GetChild(0);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmosSelected()
+        {
+            var tmp = Gizmos.color;
+            Gizmos.color = new Color(1f, 0f, 0f, 0.8f);
+            var childTransform = EditorApplication.isPlaying ? child : GetChild();
+            var length = EditorApplication.isPlaying ? Value.springLength : Vector3.Distance(transform.position, childTransform.position);
+            Gizmos.DrawMesh(debugMesh, transform.position, transform.rotation, Vector3.one * radius + boneAxis * length);
+            Gizmos.color = tmp;
+        }
+#endif
+    }
+}
